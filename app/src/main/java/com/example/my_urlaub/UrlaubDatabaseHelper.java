175b35c5c3e94db7a96d624b1056a05b141f3c5a
con-mickos <110124200+con-mickos@users.noreply.github.com>
2022-09-23 15:54:02 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/my_urlaub/UrlaubDatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.my_urlaub.room;
+
+import android.content.Context;
+import androidx.room.Room;
+import com.example.my_urlaub.Urlaub;
+import java.util.ArrayList;
+
+public class UrlaubDatabaseHelper {
+
+    private static final String DATABASE_NAME = "urlaube-db";
+    private final Context context;
+    private UrlaubDatabase db;
+
+    public UrlaubDatabaseHelper(Context context){
+        this.context = context;
+        initDatabase();
+    }
+
+    private void initDatabase(){
+        db = Room.databaseBuilder(context, UrlaubDatabase.class, DATABASE_NAME)
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public void addUrlaub(Urlaub urlaub) {
+        db.urlaubDAO().insertUrlaub(urlaub);
+    }
+
+    public void updateUrlaub(Urlaub urlaub) {
+        db.urlaubDAO().updateUrlaub(urlaub);
+    }
+
+    public ArrayList<Urlaub> getAllUrlaube(){
+        return new ArrayList<Urlaub>(db.urlaubDAO().getAllUrlaube());
+    }
+
+}
+
